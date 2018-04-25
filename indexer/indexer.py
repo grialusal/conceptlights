6196eca9713da21d0900cff717fa7b0645cfa1f2
Alex Benito <abenito@usal.es>
2018-04-25 17:18:02 +0200

    Completed indexer.py. Now extracting geo_points when available

--- indexer/indexer.py
+++ indexer/indexer.py
@@ -4,6 +4,7 @@ from elasticsearch import Elasticsearch
 from elasticsearch.helpers import bulk
 from bs4 import BeautifulSoup
 import pymysql
+import json
 
 
 print("Connecting to ES...")
@@ -25,13 +26,24 @@ if es.indices.exists(index='dboe'):
 	if es.indices.delete(index='dboe'):
 		print('dboe index deleted, will reindex now.')
 
-body = {"settings" : {
-		"index": { "number_of_shards": 1,
-					"number_of_replicas": 0
+body = {
+			"settings" : {
+				"number_of_shards": 1,
+				"number_of_replicas": 0
+			},
+			"mappings": {
+				"dboe-type": {
+						"properties": {
+							"location" : {
+								"type" : "geo_point"
+					}
 				}
-	}}
+			}
+		}}
 
 es.indices.create( index='dboe', ignore=400, body=body )
+
+db_cur = conn.cursor()
 
 actions = []
 
@@ -45,15 +57,35 @@ for root, dirs, files in os.walk(rootPath):
 		soup = BeautifulSoup(open(os.path.join(root, filename), "r", encoding="utf-8"), 'xml')
 		for entry in soup.find_all("entry"):
 			usg = entry.find('usg')
-			if not usg or len(list(usg.descendants)) < 5:
-				print("Could not find usg or it does not contain enough info")
+			if not usg:
 				continue
-			for child in usg.descendants:
-				print(child)
-
-			continue
+			else:
+				place = usg.find("place", {"type" : "Ort"})
+				if not place:
+					continue
 
 			entryObj = {}
+			entryObj['placeName'] = place.placeName.string
+			entryObj['sigle'] = place.idno.string
+
+			point = None
+			query = "SELECT ST_AsGeoJSON(dboe_1.GISort.the_geom) AS geom FROM dboe_1.GISort WHERE dboe_1.GISort.name='{}'".format(place.placeName.string)
+			number_of_rows = db_cur.execute(query)
+			if number_of_rows > 0:
+				point = db_cur.fetchone()['geom']
+			else:
+				query = "SELECT  ST_AsGeoJSON(dboe_1.GISort.the_geom) AS geom  FROM dboe_1.GISort" \
+			+ " INNER JOIN dboe_1.ort ON  dboe_1.GISort.id = dboe_1.ort.gis_ort_id where dboe_1.ort.originaldaten like '%{}%'".format(place.idno.string)
+				number_of_rows = db_cur.execute(query)
+				if number_of_rows > 0:
+					point = db_cur.fetchone()['geom']
+			
+			if point:
+				pointObj = json.loads(point)
+				entryObj['location'] = {'lat': pointObj['coordinates'][1], 'lon': pointObj['coordinates'][0]}
+			else:
+				continue
+
 			entryObj['main_lemma'] = str(entry.form.orth.string)
 			if len(entryObj['main_lemma']) == 0:
 				continue
@@ -92,9 +124,7 @@ for root, dirs, files in os.walk(rootPath):
 					'_source': entryObj
 			})
 			if len(actions) > 50:
-				print('indexing 50 entries...')
 				bulk(es, actions)
-				print('indexed')
 				actions = []
-
+print('Done')
 exit(0)
