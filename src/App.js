e84d6dfd4a1fe11d8e1835c4f65994b1e6d053df
Alex Benito <abenito@usal.es>
2018-05-23 11:35:47 +0200

    Basic map layout and testing of aggregations

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ReactiveBase, ReactiveComponent, SingleList } from '@appbaseio/reactivesearch'
+import { ReactiveBase, ReactiveComponent, SingleList, TextField } from '@appbaseio/reactivesearch'
 import { ReactiveMap } from '@appbaseio/reactivemaps';
 
 import ReactDOM from "react-dom";
@@ -20,26 +20,87 @@ class App extends Component {
 				mapKey="AIzaSyAe92NwI94op_5edlU9AH0XzHOP2cSqh6M">
 				<div className="row">
 					<div className="col">
-						<SingleList
-							title="Questionnaires"
-							componentId="questionnaires"
-							dataField="questionnaire.keyword"
-							size={50}
-							showSearch={true}/>
+						<div className="row">
+							<TextField
+								title="Main Lemma"
+							  	componentId="LemmaTextField"
+							  	dataField="main_lemma"
+							  	placeholder="Type a lemma"
+							  	showClear={true}
+							  	beforeValueChange={
+								    function(value) {
+								      // called before the value is set
+								      // returns a promise
+								      return new Promise((resolve, reject) => {
+								        // update state or component props
+								        resolve()
+								        // or reject()
+								      })
+								    }
+								  }
+								onValueChange={
+								    function(value) {
+								      console.log("current value: ", value)
+								      // set the state
+								      // use the value with other js code
+								    }
+								  }
+								onQueryChange={
+								    function(prevQuery, nextQuery) {
+								      // use the query with other js code
+								      console.log('prevQuery', prevQuery);
+								      console.log('nextQuery', nextQuery);
+								    }
+								  }
+							/>
+						</div>
+						<div className="row">
+							<SingleList
+								title="Questionnaires"
+								componentId="QuestionnaireList"
+								dataField="questionnaire.keyword"
+								size={10}
+								showSearch={true}
+								onValueChange={
+								    function(value) {
+								      console.log("current value: ", value)
+								      // set the state
+								      // use the value with other js code
+								    }
+								}
+								onQueryChange={
+								    function(prevQuery, nextQuery) {
+								      // use the query with other js code
+								      console.log('prevQuery', prevQuery);
+								      console.log('nextQuery', nextQuery);
+								    }
+								}
+							/>
+						</div>
 					</div>
 					<div className="col">
 						<ReactiveComponent
 							componentId="CountryMap"
 							defaultQuery={() => ({
 								aggs: {
-									'questionnaire': {
+									gemeinde: {
 										terms: {
-											field: 'questionnaire.keyword',
-											size: 10,
+											field: 'gemeinde.keyword',
+											size: 1000,
 										},
+										aggs: {
+											'questionnaire': {
+												terms: {
+													field: 'questionnaire_title.keyword',
+												},
+											}
+										}
 									},
 								},
 							})}
+							react={{
+						      "and": ["QuestionnaireList", "LemmaTextField"]
+						    }}
 						>
 							<CountryMap />
 						</ReactiveComponent>
@@ -49,6 +110,5 @@ class App extends Component {
 		);
 	}
 }
-
 
 export default App;
