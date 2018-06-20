2b02814330f357d1294f0dfbf94cb1ea3df301ec
Alex Benito <abenito@usal.es>
2018-06-20 18:04:43 +0200

    Modified default stroke opacity in map

--- src/App.js
+++ src/App.js
@@ -55,6 +55,32 @@ class App extends Component {
 							  title="Grammatical form"
 							/>
 
+							<ReactiveComponent 
+								componentId="BubbleSelector"
+								defaultQuery={() => ({
+									aggs: {
+										questionnaire_number: {
+											terms: {
+												field: 'questionnaire_number.keyword',
+												size: 100000
+											},
+											aggs: {
+												labels: {
+													terms: {
+														field: 'questionnaire_label.keyword',
+													},
+												}
+											}
+										}
+									},
+								})}
+								react={{
+							      "and": ["LemmaTextField", "SenseTextField", "PosDropdown"]
+							    }}
+							>
+								
+								<BubbleGraph/>
+							</ReactiveComponent>
 						</div>
 
 						<div className="main">
@@ -113,7 +139,7 @@ class App extends Component {
 								react={{
 							      "and": ["LemmaTextField", "SenseTextField", "PosDropdown"]
 							    }}>
-								<ForceGraph width={1400} height={450}/>
+								<ForceGraph width={1400} height={300}/>
 							</ReactiveComponent>
 						</div>
 						<div className="footer">Footer</div>
